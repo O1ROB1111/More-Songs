30833f99f1ede34f86abcdc4d7256c3c106ad0e5
FellTomato6998 <oliverback.robaczewskiup@gmail.com>
2026-01-21 19:55:12 +0000

    Initial commit

---
+++ src/client/java/me/FellTomato6998/moresongsrealnum_5/client/Moresongsrealnum_5Client.java
@@ -0,0 +1,10 @@
+package me.FellTomato6998.moresongsrealnum_5.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Moresongsrealnum_5Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/FellTomato6998/moresongsrealnum_5/Moresongsrealnum_5.java
@@ -0,0 +1,10 @@
+package me.FellTomato6998.moresongsrealnum_5;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Moresongsrealnum_5 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
